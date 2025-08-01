f5012b540cdb7febc5d02174945f57f36d1fb621
divyabharathirekhapalli <divyabharathi777@gmail.com>
2025-08-02 01:23:39 +0530

    Add sample K6 test script (sample.test.js)

---
+++ sample.test.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 1,
+    duration: '10s',
+};
+
+export default function () {
+    const res = http.get('https://test.k6.io');
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+        'body contains "Collection"': (r) => r.body.includes('Collection'),
+    });
+    sleep(1);
+}
